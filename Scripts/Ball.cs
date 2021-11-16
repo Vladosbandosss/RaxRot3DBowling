5fb6ac28ad0ebbac3867e61d9859059715edfc9e
Vladosbandosss <dasistperfektos@gmail.com>
2021-11-16 19:58:58 +0300

    RP
    RP

---
+++ Scripts/Ball.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    private bool thrown = false;
+    public float horizontalSpeed;
+
+    private float maxX = 0.7f;
+    private float minX = -0.7f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        BallMovement();
+        ChekPos();
+    }
+
+    private void ChekPos()
+    {
+        Vector3 temp = transform.position;
+
+        temp.x = Mathf.Clamp(transform.position.x, minX, maxX);
+
+        transform.position = temp;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrown && transform.position.y<0f)//rb.IsSleeping()
+        {
+            SceneManager.LoadScene("GamePlay");
+        }
+       
+    }
+
+    void BallMovement()
+    {
+        if (!thrown)
+        {
+            float xAxis = Input.GetAxis("Horizontal");
+
+            Vector3 position = transform.position;
+            position.x += xAxis * horizontalSpeed;
+            transform.position = position;
+        }
+
+        if(!thrown && Input.GetKeyDown(KeyCode.Space))
+        {
+            thrown = true;
+            rb.isKinematic = false;
+            rb.velocity = new Vector3(0f, 0f, speed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pin")
+        {
+            Debug.Log("педорахзнули");
+        }
+    }
+
+
+}
---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float distanse;
+    public float height;
+    public GameObject objectToFollow;
+
+    private void LateUpdate()
+    {
+        if (objectToFollow == null)
+        {
+            return;
+        }
+
+        Vector3 destination = objectToFollow.transform.position;
+
+        destination.x = 0f;
+        destination.y += height;
+        destination.z += distanse;
+
+        transform.position = destination;
+    }
+}
